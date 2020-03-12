4dca79bd4140477fe864b7d777d9dce2639c087a
aerospeace <aerospeace@gmail.com>
2020-03-12 10:39:05 +0100

    Moved to an environment variable setup compatible with token renewal

--- spoticonnect/main.py
+++ spoticonnect/main.py
@@ -3,39 +3,70 @@ import datetime
 import sys
 import spotipy
 import click
+from appdirs import AppDirs
+from spoticonnect import __name__ as app_name
+from pathlib import Path
+
+
+# Set up default cache location to be used as default to the click group
+app_dirs = AppDirs(app_name)
+user_cache_dir = app_dirs.user_cache_dir
+default_cache_file = f"{user_cache_dir}/cache"
+scope = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
 
 
 @click.group(invoke_without_command=True)
-@click.option('--token', envvar='SPOTICONNECT_TOKEN')
+@click.option('--cache-file', default=default_cache_file, type=click.Path(file_okay=True, dir_okay=False))
+@click.option('--username', envvar='SPOTIPY_USERNAME', required=True,
+              help='Environment variable: SPOTIPY_USERNAME')
+@click.option('--client-id', envvar='SPOTIPY_CLIENT_ID', required=True,
+              help='Environment variable: SPOTIPY_CLIENT_ID')
+@click.option('--client-secret', envvar='SPOTIPY_CLIENT_SECRET', required=True,
+              help='Environment variable: SPOTIPY_CLIENT_SECRET')
+@click.option('--redirect-uri', envvar='SPOTIPY_REDIRECT_URI', required=True,
+              help='Environment variable: SPOTIPY_REDIRECT_URI')
 @click.pass_context
-def cli(ctx, token):
+def cli(ctx, cache_file, username, client_id, client_secret, redirect_uri):
     ctx.ensure_object(dict)
+    ctx.obj['cache_file'] = cache_file
+    ctx.obj['username'] = username
+    ctx.obj['client_id'] = client_id
+    ctx.obj['client_secret'] = client_secret
+    ctx.obj['redirect_uri'] = redirect_uri
     if ctx.invoked_subcommand != 'get-token':
-        if not token:
-            click.echo('Please run with subcommand get-token to setup the required token')
+        # Weak check whether needs to be initialised: just checking whether the cache file exists!
+        if not Path(cache_file).is_file():
+            click.echo('Please run with subcommand get-token to setup the required token and cache path')
             sys.exit(2)
-        # else there is a token
         else:
+            token = spotipy.util.prompt_for_user_token(scope=scope,
+                                                       username=username,
+                                                       client_id=client_id,
+                                                       client_secret=client_secret,
+                                                       redirect_uri=redirect_uri,
+                                                       cache_path=cache_file)
             sp = spotipy.Spotify(auth=token)
             ctx.obj['sp'] = sp
 
 
 @cli.command()
-@click.option('--username', prompt=True)
-@click.option('--client-id', prompt=True)
-@click.option('--client-secret', prompt=True)
-@click.option('--redirect-uri', prompt=True)
-def get_token(username, client_id, client_secret, redirect_uri):
-    scope = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
-    token = spotipy.util.prompt_for_user_token(scope=scope,
-                                               username=username,
-                                               client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri)
-    click.echo("Please add an environment variable SPOTICONNECT_TOKEN with the value of the following toke:")
-    click.echo(token)
-    click.echo("For instance, add in your .zshenv:")
-    click.echo(f"export SPOTICONNECT_TOKEN={token}")
+@click.pass_context
+def get_token(ctx):
+    cache_file = ctx.obj['cache_file']
+    username = ctx.obj['username']
+    client_id = ctx.obj['client_id']
+    client_secret = ctx.obj['client_secret']
+    redirect_uri = ctx.obj['redirect_uri']
+    spotipy.util.prompt_for_user_token(scope=scope,
+                                       username=username,
+                                       client_id=client_id,
+                                       client_secret=client_secret,
+                                       redirect_uri=redirect_uri,
+                                       cache_path=cache_file)
+    # click.echo("Please add an environment variable SPOTICONNECT_TOKEN with the value of the following toke:")
+    # click.echo(token)
+    # click.echo("For instance, add in your .zshenv:")
+    # click.echo(f"export SPOTICONNECT_TOKEN={token}")
 
 @cli.command()
 @click.option('--query-type',
@@ -134,6 +165,9 @@ def toggle_repeat(ctx):  # not the command becomes toggle-shuffle
 def status(ctx, formatting):
     sp = ctx.obj['sp']
     currently_playing = sp.currently_playing()
+    if currently_playing is None:
+        click.echo("NA")
+        return
     format_dict = {'status': 'playing' if currently_playing['is_playing'] else 'stopped',
                    'artist': currently_playing['item']['artists'][0]['name'],
                    'album': currently_playing['item']['album']['name'], 'track': currently_playing['item']['name'],
